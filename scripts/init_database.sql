775df75d8e530e6bddc2b5441f2269bc329ff831
Leva1991 <levgoal1991@gmail.com>
2025-08-24 23:46:22 +0400

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,23 @@
+use master;
+
+GO
+
+-- Create the "DataWeareHouse"
+CREATE DATABASE DataWareHouse;
+
+GO
+
+use DataWareHouse;
+
+GO
+
+CREATE SCHEMA bronze;
+
+Go
+
+CREATE SCHEMA silver;
+
+GO
+
+
+CREATE SCHEMA gold;
